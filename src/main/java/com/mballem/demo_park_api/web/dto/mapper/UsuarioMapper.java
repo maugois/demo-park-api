2f2e8c5a61437938fd762937f2938001254e8c9f
maugois <gois.mauricio12@gmail.com>
2026-01-26 16:25:35 -0300

    feat: Criando os recursos do usuário, criando e usando DTO para os recursos do usuário com o auxílio do Mapper para conversão

---
+++ src/main/java/com/mballem/demo_park_api/web/dto/mapper/UsuarioMapper.java
@@ -0,0 +1,46 @@
+package com.mballem.demo_park_api.web.dto.mapper;
+
+import com.mballem.demo_park_api.entity.Usuario;
+import com.mballem.demo_park_api.web.dto.UsuarioCreateDto;
+import com.mballem.demo_park_api.web.dto.UsuarioResponseDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.TypeMap;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsuarioMapper {
+    public static Usuario toUsuario(UsuarioCreateDto createDto) {
+        return new ModelMapper().map(createDto, Usuario.class);
+    }
+
+    public static UsuarioResponseDto toDto(Usuario usuario) {
+        String role = usuario.getRole().name().substring("ROLE_".length());
+        PropertyMap<Usuario, UsuarioResponseDto> props = new PropertyMap<Usuario, UsuarioResponseDto>() {
+            @Override
+            protected void configure() {
+                map().setRole(role);
+            }
+        };
+        ModelMapper mapper = new ModelMapper();
+        mapper.addMappings(props);
+        return mapper.map(usuario, UsuarioResponseDto.class);
+    }
+
+   /* public static UsuarioResponseDto toDto(Usuario usuario) {
+        String role = usuario.getRole().name().substring("ROLE_".length());
+        ModelMapper mapperMain = new ModelMapper();
+        TypeMap<Usuario, UsuarioResponseDto> propertyMapper =
+                mapperMain.createTypeMap(Usuario.class, UsuarioResponseDto.class);
+        propertyMapper.addMappings(
+                mapper -> mapper.map(src -> role, UsuarioResponseDto::setRole)
+        );
+        return mapperMain.map(usuario, UsuarioResponseDto.class);
+    }
+    */
+
+    public static List<UsuarioResponseDto> toListDto(List<Usuario> usuarios) {
+        return usuarios.stream().map(user -> toDto(user)).collect(Collectors.toList());
+    }
+}
