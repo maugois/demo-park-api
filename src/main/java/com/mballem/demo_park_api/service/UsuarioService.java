9a9096465ea584e0fb61367dc4b6d8b2547b3bb9
maugois <gois.mauricio12@gmail.com>
2026-01-26 11:13:49 -0300

    feat: add recursos/endpoint do Usuario

---
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,40 @@
+package com.mballem.demo_park_api.service;
+
+import com.mballem.demo_park_api.entity.Usuario;
+import com.mballem.demo_park_api.repository.UsuarioRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+}
